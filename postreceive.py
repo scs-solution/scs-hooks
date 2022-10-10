efdbdde8c1940f0d3cc35580ae30168f2e961df9
rollrat <rollrat.cse@gmail.com>
2022-10-10 22:13:07 +0900

    postreceive: fix backend script

--- postreceive.py
+++ postreceive.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/postreceive_backend', methods=['POST'])
 def back():
-    os.system("./backend-ci-hooks/candy-server-spring.sh")
+    os.system("./candy-server-spring.sh")
     return 'Success!'
 
 
